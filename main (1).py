c1da5eaecdfc691c38dc02fc2036d3c05e4dbbdb
Lucas de Queiroz <63605898+QIRoss@users.noreply.github.com>
2022-03-03 18:54:55 -0300

    Update main (1).py

--- main (1).py
+++ main (1).py
@@ -3,6 +3,7 @@ from scipy.linalg import toeplitz
 import matplotlib.pyplot as plt
 import math
 import random
+import pandas as pd
 
 def generateRandomBPSKvector(n):
   symbols = [-1,1]
@@ -18,15 +19,15 @@ def generateRandomQAM4vector(n):
     vector.append(random.choice(symbols))
   return np.array(vector)
 
-def generateWhiteNoiseVector(n):
+def generateWhiteNoiseVector(n, std=1):
   mean = 0
-  std = 1 
+  std = random.uniform(0.14, 1)
   num_samples = n
   vector = np.random.normal(mean, std, size=num_samples)
   return vector 
 
 L = 41
-M = 32
+M = 8
 P = M + L - 1
 
 def generateH():
@@ -56,11 +57,11 @@ def QAM4Estimator(M):
   vector = []
   for i in M:
     angle_i = np.angle(i)
-    if angle_i > 0 and angle_i < math.pi/2:
+    if angle_i >= 0 and angle_i <= math.pi/2:
       vector.append(1+1j)
-    elif angle_i > math.pi/2 and angle_i < math.pi:
+    elif angle_i >= math.pi/2 and angle_i <= math.pi:
       vector.append(-1+1j)
-    elif angle_i < 0 and angle_i > -math.pi/2:
+    elif angle_i <= 0 and angle_i >= -math.pi/2:
       vector.append(1-1j)
     else:
       vector.append(-1-1j)
@@ -76,47 +77,45 @@ def calculateErrorProbability(tx, rx):
 def calculateSNR(signal, noise):
   return np.var(signal)/np.var(noise)
 
-y = np.empty(10000)
-x = np.empty(10000)
+aaa = np.empty(shape=(10000,2))
 
 for i in range(0,10000):
   H   = generateH()
   un  = generateRandomBPSKvector(M)
-  #un  = generateRandomQAM4vector(M)
-  #un  = np.transpose(un)
   v0n = generateWhiteNoiseVector(P)
   zn  = np.dot(H,un)
   y0n = zn + v0n
   K   = np.linalg.pinv(H)
   eSn = np.dot(K,y0n)
   iSn = BPSKEstimator(eSn)
-  #iSn = QAM4Estimator(eSn)
   errorProbability = calculateErrorProbability(un,iSn)
   SNR              = calculateSNR(zn,v0n)
-  y[i] = errorProbability
-  x[i] = SNR
+  aaa[i][0] = np.round(SNR,1)
+  aaa[i][1] = errorProbability
 
-plt.plot(x,y,'o')
-plt.savefig('resultBPSK.png')
+aaa1 = pd.DataFrame(aaa).groupby(0, as_index=False)[1].mean().values.tolist()
+aaa2 = [i[1] for i in aaa1]
+aaa3 = [i[0] for i in aaa1]
 
-plt.clf()
+plt.plot(aaa3,aaa2, color='blue')
 
 for i in range(0,10000):
   H   = generateH()
-  #un  = generateRandomBPSKvector(M)
   un  = generateRandomQAM4vector(M)
-  #un  = np.transpose(un)
   v0n = generateWhiteNoiseVector(P)
   zn  = np.dot(H,un)
   y0n = zn + v0n
   K   = np.linalg.pinv(H)
   eSn = np.dot(K,y0n)
-  #iSn = BPSKEstimator(eSn)
   iSn = QAM4Estimator(eSn)
   errorProbability = calculateErrorProbability(un,iSn)
   SNR              = calculateSNR(zn,v0n)
-  y[i] = errorProbability
-  x[i] = SNR
+  aaa[i][0] = np.round(SNR,1)
+  aaa[i][1] = errorProbability
 
-plt.plot(x,y,'o')
-plt.savefig('resultQAM4.png')
+aaa1 = pd.DataFrame(aaa).groupby(0, as_index=False)[1].mean().values.tolist()
+aaa2 = [i[1] for i in aaa1]
+aaa3 = [i[0] for i in aaa1]
+
+plt.plot(aaa3,aaa2,color='red')
+plt.savefig('Result.png')
